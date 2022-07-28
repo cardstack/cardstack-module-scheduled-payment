ae89001918436c49c583d4c74e4796a742881bfc
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2022-07-28 14:41:14 +0700

    Initialize project structure

---
+++ hardhat.config.ts
@@ -0,0 +1,81 @@
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "solidity-coverage";
+import "hardhat-deploy";
+import dotenv from "dotenv";
+import type { HttpNetworkUserConfig } from "hardhat/types";
+import yargs from "yargs";
+
+const argv = yargs
+  .option("network", {
+    type: "string",
+    default: "hardhat",
+  })
+  .help(false)
+  .version(false)
+  .parseSync();
+
+// Load environment variables.
+dotenv.config();
+const { INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK } = process.env;
+
+const DEFAULT_MNEMONIC =
+  "test test test test test test test test test test test junk";
+
+const sharedNetworkConfig: HttpNetworkUserConfig = {};
+if (PK) {
+  sharedNetworkConfig.accounts = [PK];
+} else {
+  sharedNetworkConfig.accounts = {
+    mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+  };
+}
+
+if (["rinkeby", "mainnet"].includes(argv.network) && INFURA_KEY === undefined) {
+  throw new Error(
+    `Could not find Infura key in env, unable to connect to network ${argv.network}`
+  );
+}
+
+export default {
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    deploy: "src/deploy",
+    sources: "contracts",
+  },
+  solidity: {
+    compilers: [{ version: "0.8.9" }],
+  },
+  networks: {
+    mainnet: {
+      ...sharedNetworkConfig,
+      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+    },
+    rinkeby: {
+      ...sharedNetworkConfig,
+      url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+    },
+    xdai: {
+      ...sharedNetworkConfig,
+      url: "https://rpc.gnosischain.com",
+    },
+    sokol: {
+      ...sharedNetworkConfig,
+      url: "https://sokol.poa.network",
+    },
+    matic: {
+      ...sharedNetworkConfig,
+      url: "https://rpc-mainnet.maticvigil.com",
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  mocha: {
+    timeout: 2000000,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+};
